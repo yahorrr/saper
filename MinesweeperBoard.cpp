14061f96121290b916a576bda5a361454e08c073
Yahor Lazrevich <288490@student.pwr.edu.pl>
2025-03-20 22:39:35 +0100

    separate files

--- MinesweeperBoard.cpp
+++ MinesweeperBoard.cpp
@@ -1,3 +1,53 @@
-//
-// Created by user on 20.03.2025.
-//
+#include "MinesweeperBoard.h"
+
+#include <iostream>
+#include <vector>
+#include "Array2D.h"
+
+using std::cin;
+using std::cout;
+using std::endl;
+using std::string;
+using std::vector;
+
+MinesweeperBoard::MinesweeperBoard()
+{
+    width = 5;
+    height = 7;
+    board.resize(height, width);
+    clearBoard();
+
+    board[0][0] = {true, false, false};
+    board[1][1] = {false, false, true};
+    board[0][2] = {true, true, false};
+}
+
+void MinesweeperBoard::clearBoard()
+{
+    for (int ncol = 0; ncol < width; ncol++) {
+        for (int nrow = 0; nrow < height; nrow++) {
+            board[nrow][ncol] = {false, false, false};
+        }
+    }
+}
+
+void MinesweeperBoard::debug_display() const {
+    cout << "    ";
+    for (int col = 0; col < this->width; col++) {
+        cout << col << "     ";
+    }
+    cout << "\n";
+
+    for (int row = 0; row < this->height; row++) {
+        cout << row << " ";
+        for (int col = 0; col < this->width; col++) {
+            char mina = board[row][col].hasMine ? 'M' : '.';
+            char odkryte = board[row][col].isRevealed ? 'o' : '.';
+            char flaga = board[row][col].hasFlag ? 'f' : '.';
+
+            std::cout << "[" << mina << odkryte << flaga << "] ";
+        }
+        std::cout << "\n";
+
+    }
+}
--- MinesweeperBoard.h
+++ MinesweeperBoard.h
@@ -1,4 +1,33 @@
 #ifndef MINESWEEPERBOARD_H
 #define MINESWEEPERBOARD_H
 
+#include "Array2D.h"
+
+using std::cin;
+using std::cout;
+using std::endl;
+using std::string;
+using std::vector;
+
+struct Field
+{
+    bool hasMine;
+    bool hasFlag;
+    bool isRevealed;
+};
+
+enum GameMode  { DEBUG, EASY, NORMAL, HARD };
+
+class MinesweeperBoard
+{
+    Array2D <Field> board = Array2D<Field>(0, 0);
+    int width;
+    int height;
+    void clearBoard();
+
+public:
+    MinesweeperBoard();
+    MinesweeperBoard(int width, int height, GameMode mode);
+    void debug_display() const;
+};
 #endif //MINESWEEPERBOARD_H
